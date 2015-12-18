c929183c4b46cc9012d7315f4a843eb2664bf129
Dan Bennett <naebeth@googlemail.com>
2015-12-18 12:02:21 +0000

    add autoloader

--- src/Forms/autoload.php
+++ src/Forms/autoload.php
@@ -1,5 +1,18 @@
 <?php
 
-namespace Forms;
-
 define( "FORMS_ROOT", dirname( __FILE__ ) . "/" );
+
+function loader( $_class ) {
+	$class = explode( '\\', $_class );
+	
+	$file = array_pop( $class );
+	$dir = array_pop( $class );
+	$class = FORMS_ROOT . $dir . "/" . $file . ".php";
+	
+	if( file_exists( $class ) ) {
+		require_once $class;
+		return;
+	}
+}
+
+spl_autoload_register( 'loader' );

6d797ae1fbee4e353ac39b33a4bc8d52e445cf87
Kieron <info@kieronwilson.co.uk>
2016-06-27 14:33:29 +0100

    updates

--- spec/Forms/Elements/ButtonSpec.php
+++ spec/Forms/Elements/ButtonSpec.php
@@ -5,28 +5,32 @@ namespace spec\Forms\Elements;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
-class ButtonSpec extends ObjectBehavior {
+class ButtonSpec extends ObjectBehavior
+{
 
-	public function let() {
-		$this->beAnInstanceOf('Forms\Elements\Button');
-	}
+    public function let()
+    {
+        $this->beAnInstanceOf('Forms\Elements\Button');
+    }
 
-	public function it_should_be_a_type_of_button() {
-		$this->beConstructedWith('foo');
+    public function it_should_be_a_type_of_button()
+    {
+        $this->beConstructedWith('foo');
 
-		$this->getType()->shouldBe('button');
-	}
+        $this->getType()->shouldBe('button');
+    }
 
-	public function it_should_return_the_value() {
-		$this->beConstructedWith('foo');
+    public function it_should_return_the_value()
+    {
+        $this->beConstructedWith('foo');
 
-		$this->getValue()->shouldBe('');
-	}
+        $this->getValue()->shouldBe('');
+    }
 
-	public function it_should_set_the_value() {
-		$this->beConstructedWith('foo');
+    public function it_should_set_the_value()
+    {
+        $this->beConstructedWith('foo');
 
-		$this->withValue('bar')->getValue()->shouldBe('bar');
-	}
-
+        $this->withValue('bar')->getValue()->shouldBe('bar');
+    }
 }
--- spec/Forms/FormSpec.php
+++ spec/Forms/FormSpec.php
@@ -5,44 +5,52 @@ namespace spec\Forms;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
-class FormSpec extends ObjectBehavior {
+class FormSpec extends ObjectBehavior
+{
 
-    public function it_is_initializable() {
-		$this->shouldHaveType('Forms\Form');
-	}
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('Forms\Form');
+    }
 
-	public function it_should_accept_a_new_element($element) {
-		$element->beADoubleOf('Forms\Elements\Input');
-		$this->append($element);
-		$this->count()->shouldReturn(1);
-	}
+    public function it_should_accept_a_new_element($element)
+    {
+        $element->beADoubleOf('Forms\Elements\Input');
+        $this->append($element);
+        $this->count()->shouldReturn(1);
+    }
 
-	public function it_should_fetch_a_element_by_name($element) {
-		$element->beADoubleOf('Forms\Elements\Input');
-		$element->getName()->willReturn('test');
-		$this->append($element);
-		$this->getElement('test')->shouldBe($element);
-	}
+    public function it_should_fetch_a_element_by_name($element)
+    {
+        $element->beADoubleOf('Forms\Elements\Input');
+        $element->getName()->willReturn('test');
+        $this->append($element);
+        $this->get('test')->shouldBe($element);
+    }
 
-	public function it_should_throw_exception_for_missing_elements() {
-		$this->shouldThrow(new \Forms\Exceptions\FormElementNotFound('form element not found test'))->during('getElement', ['test']);
-	}
+    public function it_should_throw_exception_for_missing_elements()
+    {
+        $this->shouldThrow(new \Forms\Exceptions\FormElementNotFound('form element not found test'))->during('getElement', ['test']);
+    }
 
-	public function it_should_return_empty_for_missing_attributes() {
-		$this->getAttribute('foo')->shouldBe('');
-	}
+    public function it_should_return_a_empty_string_for_missing_attributes()
+    {
+        $this->getAttribute('foo')->shouldBe('');
+    }
 
-	public function it_should_return_attribute_when_set_by_key_value() {
-		$this->withAttribute('foo', 'bar')->getAttribute('foo')->shouldBe('bar');
-	}
+    public function it_should_return_attribute_when_set_by_key_value()
+    {
+        $this->withAttribute('foo', 'bar')->getAttribute('foo')->shouldBe('bar');
+    }
 
-	public function it_should_return_attribute_when_set_with_array_of_attributes() {
-		$this->withAttributes(['baz' => 'qux'])->getAttribute('baz')->shouldBe('qux');
-	}
+    public function it_should_return_attribute_when_set_with_array_of_attributes()
+    {
+        $this->withAttributes(['baz' => 'qux'])->getAttribute('baz')->shouldBe('qux');
+    }
 
-	public function it_should_return_attributes_as_a_string() {
-		$this->setAttributes(['foo' => 'bar']);
-		$this->getAttributesAsString()->shouldBe('foo="bar"');
-	}
-
+    public function it_should_return_attributes_as_a_string()
+    {
+        $this->setAttributes(['foo' => 'bar']);
+        $this->getAttributesAsString()->shouldBe('foo="bar"');
+    }
 }

4114cc1229167901810a7d3e1ed6fe99d4ac37a6
Kieron <info@kieronwilson.co.uk>
2016-06-27 18:23:59 +0100

    fix types

--- src/Traits/Metadata.php
+++ src/Traits/Metadata.php
@@ -6,9 +6,14 @@ trait Metadata
 {
     protected $meta = [];
 
-    public function getMeta(string $key, $default = null): string
+    public function getMeta(string $key, $default = ''): string
     {
         return array_key_exists($key, $this->meta) ? $this->meta[$key] : $default;
+    }
+
+    public function hasMeta(string $key): bool
+    {
+        return array_key_exists($key, $this->meta);
     }
 
     public function setMeta(string $key, string $value)
